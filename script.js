46ee119c8e5eda1375266219924e1a882b912205
Dennis Ferreira <ferreira.dennis02@gmail.com>
2021-06-02 18:26:47 -0300

    Mudança no visual | Sessão 1 funcionando com botão de iniciar e pausar.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let minutos = document.getElementById('minutos');
 let iniciar = document.getElementById('iniciar');
 let segundos = document.getElementById('segundos');
+var pause = document.getElementById('pause');
+var intervalo;
+var isPlaying = false;
 
 iniciar.addEventListener('click', handleEvent);
+pause.addEventListener('click', handleEventPause);
 
 function handleEvent(event) {
-  var intervalo = setInterval(function () {
+  isPlaying = true;
+  toggleButtons();
+  intervalo = setInterval(function () {
     let sec = getSegundos();
     let min = getMinutos();
     sec--;
@@ -19,6 +25,22 @@ function handleEvent(event) {
       atualizaSegundos(59);
     }
   }, 1000);
+}
+
+function toggleButtons() {
+  if (isPlaying) {
+    iniciar.style.visibility = 'hidden';
+    pause.style.visibility = 'visible';
+  } else {
+    pause.style.visibility = 'hidden';
+    iniciar.style.visibility = 'visible';
+  }
+}
+
+function handleEventPause(event) {
+  isPlaying = false;
+  clearInterval(intervalo);
+  toggleButtons();
 }
 
 function atualizaSegundos(num) {
